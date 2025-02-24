84efbd3590ba6e814e6e60e83ad7772b79b6531d
Gulshan Anand <gulshan2052@gmail.com>
2025-02-24 20:33:29 +0530

    caching added

--- src/main/java/com/robotlab/Shortify/Controller/CleanupController.java
+++ src/main/java/com/robotlab/Shortify/Controller/CleanupController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(("/api/v1/cleanup"))
+@RequestMapping("/api/v1/cleanup")
 public class CleanupController {
 
     private final CleanupService cleanupService;
--- src/main/java/com/robotlab/Shortify/ServiceImpl/HomeServiceImpl.java
+++ src/main/java/com/robotlab/Shortify/ServiceImpl/HomeServiceImpl.java
@@ -4,18 +4,23 @@ import com.robotlab.Shortify.Entity.UrlDetail;
 import com.robotlab.Shortify.Repository.UrlDetailRepository;
 import com.robotlab.Shortify.Service.HomeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class HomeServiceImpl implements HomeService {
 
     private final UrlDetailRepository urlDetailRepository;
 
+    @Cacheable(value = "urls", key = "#id", unless = "#result == null")
     public String getLongUrl(String id) {
+        log.info("Cache MISS - Fetching from DB for ID: {}", id);
         Optional<UrlDetail> urlDetailOptional = urlDetailRepository.findById(id);
         if(urlDetailOptional.isEmpty()) {
             return null;
